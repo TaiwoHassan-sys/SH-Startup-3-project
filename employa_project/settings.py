a75f348ab10df7191679ead3ce262602accf7fb9
roji-tech <rojitech9@gmail.com>
2022-06-24 06:09:33 +0100

    added users

--- employa_project/settings.py
+++ employa_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t-d-jft3u=p(2okx=x1j!qnx^*cwk+pl7_&9q-j25-99qpxx&('
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
